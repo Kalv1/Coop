f0526e8c73961ee4a00220c90c671700e762a87b
Kalv1 <calvin.lambert@protonmail.com>
2022-01-31 11:12:47 +0100

    :sparkles: Rediction to profil & display profil img in conv

--- src/components/MessageComponent.vue
+++ src/components/MessageComponent.vue
@@ -1,18 +1,34 @@
 <template>
   <div>
-    <h1>{{ getMember(this.id) }} : {{ message }}</h1>
+    <div v-if="this.member" class="is-flex">
+      <img class='mr-2' :src="mailtoMD5" width="20" height="20" alt="">
+      <h1><strong><router-link :to="{ name:'MembreProfil' , params:{ id: this.id }}">{{ member.fullname }}</router-link></strong> : {{ message }}</h1>
+    </div>
+    <div v-else>
+        <h1>Deleted user : {{ message }}</h1>
+    </div>
+
   </div>
 </template>
 
 <script>
+import md5 from "crypto-js/md5";
+
 export default {
   name: "MessageComponent",
   props: ['message', 'id'],
-  methods : {
-    getMember(id) {
-      console.log(id)
-      let member = this.$store.state.members.find(el => el.id === id)
-      return member.fullname
+  data() {
+    return {
+      member: {},
+    }
+  },
+  mounted() {
+      this.member = this.$store.state.members.find(el => el.id === this.id)
+  },
+  computed: {
+    mailtoMD5() {
+      let res = md5(this.member.email).toString()
+      return 'https://www.gravatar.com/avatar/' + res + '?d=identicon'
     }
   }
 }
