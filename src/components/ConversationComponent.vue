335d5175bf892f94c8c1ee132fa67cedff5c651f
Kalv1 <calvin.lambert@protonmail.com>
2022-02-04 12:27:57 +0100

    :sparkles: Conv edit & delete + reformat MessageComponent

--- src/components/ConversationComponent.vue
+++ src/components/ConversationComponent.vue
@@ -1,17 +1,25 @@
 <template>
-  <div v-if="display" class="is-flex is-justify-content-space-between box mt-5">
-    <router-link :to="{ name:'ConversationDetails' , params:{ id: this.id }}" style="text-decoration: none; color: inherit;">
-      <div>
-        <h1>{{ label }}</h1>
-        <p> {{ topic }}</p>
+  <transition name="fade">
+    <div v-if="display" class="box mt-5">
+      <div class="is-flex is-justify-content-space-between">
+        <router-link :to="{ name:'ConversationDetails' , params:{ id: this.id }}" style="text-decoration: none; color: inherit;">
+          <div>
+            <h1>{{ title }}</h1>
+            <p> {{ desc }}</p>
+          </div>
+        </router-link>
+        <div>
+          <button class="button" @click="edit = !edit"><i class="fas fa-edit"></i></button>
+          <button class="button" @click="deleteTopic"><i class="fas fa-trash-alt"></i></button>
+        </div>
       </div>
-    </router-link>
-    <div>
-      <button class="button" @click=""><i class="fas fa-edit"></i></button>
-      <button class="button" @click="deleteTopic"><i class="fas fa-trash-alt"></i></button>
+      <div v-if="edit === true">
+        <textarea class="textarea mt-5" placeholder="Ecrivez le nom du topic" rows="1" v-model="newTopic"/>
+        <textarea class="textarea mt-2" placeholder="Ecrivez le sujet du topic" rows="1" v-model="newLabel"/>
+        <button @click="sendEdit" class="button is-primary mt-2">Envoyer</button>
+      </div>
     </div>
-  </div>
-
+  </transition>
 </template>
 
 <script>
@@ -20,18 +28,35 @@ export default {
   props: ['label', 'topic', 'id'],
   data() {
     return {
-      display : true
+      display : true,
+      title : this.label,
+      desc : this.topic,
+      edit : false,
+      newTopic : '',
+      newLabel : '',
     }
   },
   methods : {
     deleteTopic(){
       this.$api.delete(`/channels/${this.id}`)
       this.display = false
+    },
+    sendEdit(){
+      this.title = this.newLabel
+      this.desc = this.newTopic
+      this.$api.put(`/channels/${this.id}`, { topic: this.newTopic, label: this.newLabel})
+      this.newTopic = ''
+      this.newLabel = ''
     }
   }
 }
 </script>
 
 <style scoped>
-
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
--- src/components/MessageComponent.vue
+++ src/components/MessageComponent.vue
@@ -1,10 +1,17 @@
 <template>
+  <transition name="fade">
   <div v-if="display" class="box ml-5">
-    <div v-if="this.member" class="is-flex">
-      <img class='mr-2' :src="mailtoMD5" width="20" height="20" alt="">
-      <h1><strong><router-link :to="{ name:'MembreProfil' , params:{ id: this.id }}">{{ member.fullname }}</router-link></strong> : <span v-html="messageNoEdit"/></h1>
-      <button v-if="id === this.$store.state.membre.id" class="ml-2 button is-small" @click="edit = !edit"><i class="fas fa-edit"></i></button>
-      <button v-if="id === this.$store.state.membre.id" class="ml-2 button is-small" @click="deleteMsg"><i class="fas fa-trash-alt"></i></button>
+    <div v-if="this.member">
+      <div class="is-flex">
+        <img class='mr-2' :src="mailtoMD5" width="20" height="20" alt="">
+        <h1><strong><router-link :to="{ name:'MembreProfil' , params:{ id: this.id }}">{{ member.fullname }}</router-link></strong> : </h1>
+        <button v-if="id === this.$store.state.membre.id" class="ml-2 button is-small" @click="edit = !edit"><i class="fas fa-edit"></i></button>
+        <button v-if="id === this.$store.state.membre.id" class="ml-2 button is-small" @click="deleteMsg"><i class="fas fa-trash-alt"></i></button>
+      </div>
+      <div class="mt-3">
+        <span v-html="messageNoEdit"/>
+      </div>
+
     </div>
     <div v-else>
         <h1>Deleted user : {{ message }}</h1>
@@ -13,8 +20,8 @@
       <textarea class="textarea mt-5" placeholder="Ecrivez votre messages ici" rows="10" v-model="messageEdit"/>
       <button @click="sendEdit" class="button is-primary mt-2">Envoyer</button>
     </div>
-
   </div>
+  </transition>
 </template>
 
 <script>
@@ -58,5 +65,10 @@ export default {
 </script>
 
 <style scoped>
-
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
